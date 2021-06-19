351a4e54463974df2b3eab0a0c164ede18a2067a
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-06-18 23:03:00 -0700

    asrge

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Book } = require('../models');
-
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
@@ -27,7 +28,54 @@ const resolvers = {
         .populate('books')
     },
   },
-  Mutaion: {
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
 
-  }
+        return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+
+        const token = signToken(user);
+        return { token, user };
+    },
+    saveBook: async (parent, { author, describtion, title, bookId, image, link}, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { author: [String], describtion: String, title: String, bookId: ID, image: String, link:String} },
+          { new: true }
+        ).populate('books');
+    
+        return updatedUser;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $unset: { books: bookdId } },
+          { new: true }
+        ).populate('books');
+    
+        return updatedUser;
+      }
+    
+      throw new AuthenticationError('You need to be logged in!');
+    }
+}
 };
